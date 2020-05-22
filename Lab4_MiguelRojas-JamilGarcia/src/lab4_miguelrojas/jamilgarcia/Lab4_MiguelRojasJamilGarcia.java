a552f1bb35ff51cae9c49da6303d00375d4914f5
Miguel Rojas <m_rojas18@unitec.edu>
2020-05-22 14:59:18 -0600

    Inicio de Sesion Terminado

--- Lab4_MiguelRojas-JamilGarcia/src/lab4_miguelrojas/jamilgarcia/Lab4_MiguelRojasJamilGarcia.java
+++ Lab4_MiguelRojas-JamilGarcia/src/lab4_miguelrojas/jamilgarcia/Lab4_MiguelRojasJamilGarcia.java
@@ -1,5 +1,6 @@
 package lab4_miguelrojas.jamilgarcia;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -8,8 +9,9 @@ public class Lab4_MiguelRojasJamilGarcia {
 
     static ArrayList<String> lista_usuarios = new ArrayList();
     static ArrayList<Ingenieros> lista_ingenieros = new ArrayList();
+    static int posicion_ing = 0;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
         Scanner sc = new Scanner(System.in);
 
@@ -26,6 +28,8 @@ public class Lab4_MiguelRojasJamilGarcia {
             switch (opcion) {
                 case 1:
                     //Registro de Ingenieros
+                    //Instanciar Objeto
+                    Ingenieros ing = new Ingenieros();
                     System.out.print("----------------\n"
                             + "Registro de Ingenieros\n\n"
                             + "Ingrese su nombre: ");
@@ -128,20 +132,99 @@ public class Lab4_MiguelRojasJamilGarcia {
                         sc = new Scanner(System.in);
                         System.out.println();
                         if (op_idioma == 1) {
-                            //Agregar a lista de idiomas
-                            //Recorrer lista de idiomas, si ya existe no agrega y manda mensaje
+                            //Buscar si el idioma ya esta en la lista de idiomas
+                            boolean add_idioma = false;
+                            for (int i = 0; i < ing.getI_Idiomas().size(); i++) {
+                                if (ing.getI_Idiomas().contains("Ingles")) {
+                                    add_idioma = true;
+                                } else {
+                                    add_idioma = false;
+                                }
+                            }
+
+                            if (add_idioma == true) {
+                                System.out.println("Ya tiene agregado el idioma.\n");
+                            } else {
+                                //Agregar el idioma
+                                ing.getI_Idiomas().add("Ingles");
+                                System.out.println("Idioma agregado exitosamente!!\n");
+                            }
                         }
                         if (op_idioma == 2) {
-                            //Agregar a lista de idiomas
+                            //Buscar si el idioma ya esta en la lista de idiomas
+                            boolean add_idioma2 = false;
+                            for (int i = 0; i < ing.getI_Idiomas().size(); i++) {
+                                if (ing.getI_Idiomas().contains("Espanol")) {
+                                    add_idioma2 = true;
+                                } else {
+                                    add_idioma2 = false;
+                                }
+                            }
+
+                            if (add_idioma2 == true) {
+                                System.out.println("Ya tiene agregado el idioma.\n");
+                            } else {
+                                //Agregar el idioma
+                                ing.getI_Idiomas().add("Espanol");
+                                System.out.println("Idioma agregado exitosamente!!\n");
+                            }
                         }
                         if (op_idioma == 3) {
-                            //Agregar a lista de idiomas
+                            //Buscar si el idioma ya esta en la lista de idiomas
+                            boolean add_idioma3 = false;
+                            for (int i = 0; i < ing.getI_Idiomas().size(); i++) {
+                                if (ing.getI_Idiomas().contains("Aleman")) {
+                                    add_idioma3 = true;
+                                } else {
+                                    add_idioma3 = false;
+                                }
+                            }
+
+                            if (add_idioma3 == true) {
+                                System.out.println("Ya tiene agregado el idioma.\n");
+                            } else {
+                                //Agregar el idioma
+                                ing.getI_Idiomas().add("Aleman");
+                                System.out.println("Idioma agregado exitosamente!!\n");
+                            }
                         }
                         if (op_idioma == 4) {
-                            //Agregar a lista de idiomas
+                            //Buscar si el idioma ya esta en la lista de idiomas
+                            boolean add_idioma4 = false;
+                            for (int i = 0; i < ing.getI_Idiomas().size(); i++) {
+                                if (ing.getI_Idiomas().contains("Japones")) {
+                                    add_idioma4 = true;
+                                } else {
+                                    add_idioma4 = false;
+                                }
+                            }
+
+                            if (add_idioma4 == true) {
+                                System.out.println("Ya tiene agregado el idioma.\n");
+                            } else {
+                                //Agregar el idioma
+                                ing.getI_Idiomas().add("Japones");
+                                System.out.println("Idioma agregado exitosamente!!\n");
+                            }
                         }
                         if (op_idioma == 5) {
-                            //Agregar a lista de idiomas
+                            //Buscar si el idioma ya esta en la lista de idiomas
+                            boolean add_idioma5 = false;
+                            for (int i = 0; i < ing.getI_Idiomas().size(); i++) {
+                                if (ing.getI_Idiomas().contains("Italiano")) {
+                                    add_idioma5 = true;
+                                } else {
+                                    add_idioma5 = false;
+                                }
+                            }
+
+                            if (add_idioma5 == true) {
+                                System.out.println("Ya tiene agregado el idioma.\n");
+                            } else {
+                                //Agregar el idioma
+                                ing.getI_Idiomas().add("Italiano");
+                                System.out.println("Idioma agregado exitosamente!!\n");
+                            }
                         }
 
                         System.out.print("Desea agregar otro idioma?\n"
@@ -150,6 +233,7 @@ public class Lab4_MiguelRojasJamilGarcia {
                                 + "Eliga una opcion: ");
                         int op_seguir = sc.nextInt();
                         sc = new Scanner(System.in);
+                        System.out.println();
 
                         if (op_seguir == 1) {
                             //do nothing
@@ -172,8 +256,19 @@ public class Lab4_MiguelRojasJamilGarcia {
 
                     SimpleDateFormat sd = new SimpleDateFormat("dd/MM/yyyy");
 
-                    //Crear el Objeto Ingeniero
+                    //Agregar el Objeto Ingeniero
+                    ing.setS_Nombre(nombre_ingeniero);
+                    ing.setS_GSang(grupo_sanguineo);
+                    ing.setS_Sexo(sexo_ing);
+                    ing.setS_Altura(altura_ing);
+                    ing.setS_Peso(peso_ing);
+                    ing.setI_Correo(correo_ing);
+                    ing.setI_Usuario(usuario_ing);
+                    ing.setI_Pass(password_ing);
+                    ing.setI_Fecha(sd.parse(fecha_nac));
+                    //Agregar a lista de Ingenieros   
                     //Agregar a lista de Ingenieros
+                    lista_ingenieros.add(ing);
                     System.out.println("Se registro exitosamente!!\n");
                     break;
                 case 2:
@@ -181,10 +276,86 @@ public class Lab4_MiguelRojasJamilGarcia {
                     System.out.print("----------------\n"
                             + "Iniciar Sesion\n\n"
                             + "Ingrese su usuario: ");
-                    String usuario = sc.nextLine();
+                    String usuario_ingresado = sc.nextLine();
                     sc = new Scanner(System.in);
                     System.out.println();
-                    break;
+                    //Verificar el usuario
+                    boolean buscar_usuario = true;
+                    for (int i = 0; i < lista_ingenieros.size(); i++) {
+                        if (lista_ingenieros.get(i).getI_Usuario().equals(usuario_ingresado)) {
+                            posicion_ing = i;
+                            buscar_usuario = false;
+                        } else {
+                            buscar_usuario = true;
+                        }
+                    }
+
+                    if (buscar_usuario == false) {
+                        //Encontro el usuario
+                        System.out.print("Ingrese su contraseña: ");
+                        String password_ingresada = sc.nextLine();
+                        sc = new Scanner(System.in);
+                        System.out.println();
+                        if (lista_ingenieros.get(posicion_ing).getI_Pass().equals(password_ingresada)) {
+                            //Contraseña correcta
+                            //Menu Todo
+
+                            boolean flag_menuP = true;
+                            while (flag_menuP) {
+                                System.out.print("Bienvenido\n\n"
+                                        + "Menu Ingenieros\n"
+                                        + "[1] Menu Seres Vivos\n"
+                                        + "[2] Menu Medios Transporte\n"
+                                        + "[3] Viajar\n"
+                                        + "[4] Salir\n"
+                                        + "Seleccione una opcion: ");
+                                int op_menu = sc.nextInt();
+                                sc = new Scanner(System.in);
+                                System.out.println();
+                                switch (op_menu) {
+                                    case 1:
+                                        //Menu seres vivos
+                                        System.out.print("------------------\n"
+                                                + "-----Menu Seres Vivos-----\n\n"
+                                                + "[1] Menu Primates\n"
+                                                + "[2] Menu Gorilas\n"
+                                                + "[3] Salir\n"
+                                                + "Seleccione una opcion: ");
+                                        break;
+                                    case 2:
+                                        //Menu medios transporte
+                                        System.out.print("------------------\n"
+                                                + "----- Menu Medios Transporte-----\n"
+                                                + "[1] Menu Helicopteros\n"
+                                                + "[2] Menu Avion Comerciales\n"
+                                                + "[3] Menu Naves Espaciales\n"
+                                                + "[4] Menu Cohetes\n"
+                                                + "[5] Salir\n"
+                                                + "Seleccione una opcion: ");
+                                        break;
+                                    case 3:
+                                        //Opcion viajar
+                                        System.out.print("-----------------\n"
+                                                + "Viajar");
+                                        break;
+                                    case 4:
+                                        //Salir de menu
+                                        System.out.println("Adios!!\n");
+                                        flag_menuP = false;
+                                        break;
+                                    default:
+                                        System.out.println("Ingrese una opcion valida.\n");
+                                }
+                            }
+
+                        } else {
+                            System.out.println("La contraseña es incorrecta.\n");
+                        }
+
+                    } else {
+                        //No encontro el usuario
+                        System.out.println("El usuario ingresado no existe o es incorrecto.\n");
+                    }
                 case 3:
                     //Fin del Programa
                     System.out.println("Fin del Programa\n");
