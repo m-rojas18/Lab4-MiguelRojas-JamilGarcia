4de50e3a7e3b2bfde9b69a99cdf7f701424a0cf4
Miguel Rojas <m_rojas18@unitec.edu>
2020-05-22 17:02:18 -0600

    Crear Primate listo

--- Lab4_MiguelRojas-JamilGarcia/src/lab4_miguelrojas/jamilgarcia/Gorila.java
+++ Lab4_MiguelRojas-JamilGarcia/src/lab4_miguelrojas/jamilgarcia/Gorila.java
@@ -8,10 +8,12 @@ public class Gorila extends Primates{
         super(); 
     }
 
-    public Gorila(int G_IQ, Medios_Transporte P_MTrans, int P_Comida, int P_CKiloM, String P_Planeta, String P_Nacimiento, String S_Nombre, String S_GSang, String S_Sexo, double S_Altura, int S_Peso) {
+    public Gorila(int G_IQ, int P_Comida, int P_CKiloM, String P_Planeta, String P_Nacimiento, String S_Nombre, String S_GSang, String S_Sexo, double S_Altura, int S_Peso) {
         super(P_Comida, P_CKiloM, P_Planeta, P_Nacimiento, S_Nombre, S_GSang, S_Sexo, S_Altura, S_Peso);
         this.G_IQ = G_IQ;
     }
+
+    
 
     public int getG_IQ() {
         return G_IQ;
