b94186ec2381e1ffe54a488dc4632ac1a7205c8c
Miguel Rojas <m_rojas18@unitec.edu>
2020-05-22 13:00:01 -0600

    Primer push

---
+++ Lab4_MiguelRojas-JamilGarcia/src/lab4_miguelrojas/jamilgarcia/Lab4_MiguelRojasJamilGarcia.java
@@ -0,0 +1,17 @@
+package lab4_miguelrojas.jamilgarcia;
+
+import java.util.Scanner;
+
+public class Lab4_MiguelRojasJamilGarcia {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Menu\n"
+                + "");
+        int opcion = sc.nextInt();
+        System.out.println();
+    }
+    
+}
